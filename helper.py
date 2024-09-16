c6cdc8323023e0cf3e93a5d09323dd966a3f7634
ADecametre <66925945+ADecametre@users.noreply.github.com>
2024-09-16 16:42:45 -0400

    Partie 1
    Partie 1 : Variables et structures de contrôle
    + Inversion de row et col pour les coordonnées

--- helper.py
+++ helper.py
@@ -1,51 +1,50 @@
-special_coins_pos = [(1, 1), (14, 1), (1, 13), (14, 13)]
-center_pos = [(12, 7), (11, 7), (13, 7), (14, 7)]
+special_coins_pos = [(1, 1), (1, 14), (13, 1), (13, 14)]
+center_pos = [(7, 11), (7, 12), (7, 13), (7, 14)]
 
 def create_board():
 
-    # TODO Create a board with the following structure
+    # [x] Create a board with the following structure
     # 1 -> Wall
     # 0 -> Path
-    
-    maze = [
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], # Top boundary
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], # Path
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], # Internal walls
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], # Paths
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], # Complex paths
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], # Open path area
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], # Narrow passage
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], # Large open path
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], # Solid wall section
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], # Path
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], # Complex wall structure
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], # More paths
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], # Internal walls
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], # Open path
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], # Extra boundary row
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], # Extra boundary row
-    ]
+
+    string_maze ="""
+████████████████
+█              █
+█ ██ ██ ███ ██ █
+█     █ █      █
+█ █ █   █ █ ██ █
+█   █ █      █ █
+█ █   ██ ███ █ █
+█ █ █      █   █
+█ █ ██ █ █ █ █ █
+█   █          █
+█ █ █ ████ ███ █
+█     █    █   █
+█ ██ ██ ██ █ █ █
+█              █
+████████████████
+████████████████
+"""
+    maze = list(map(
+        lambda ligne:[0 if char==" " else 1 for char in ligne],
+        string_maze.strip().split("\n")
+    ))
 
     return maze
 
 def create_coins(board):
     coins = []
 
-    coins.append((1, 1)) # Placeholder, ligne à retirer
-
-    # TODO: Ajouter la position de toutes les cases '0' à la variable coins. Pour ajouter un élément, vous pouvez utiliser l'expression suivante :
-    # coins.append((x, y))
-    # en remplacant x et y par la position. Notez que le premier coin est à la position (1, 1)
-
-    # TODO: Retirer les coins de chaque "coin" du carré. Vous devez utiliser la variable 'special_coins_pos' et la fonction 'remove'.
-
-    # TODO: Retirer les coins aux positions centrales, en utilisant la variable 'center_pos'.
+    # [x] Ajouter la position de toutes les cases '0' à la variable coins. Pour ajouter un élément, vous pouvez utiliser l'expression suivante : coins.append((x, y))
+    # [x] Retirer les coins de chaque "coin" du carré. Vous devez utiliser la variable 'special_coins_pos' # XXX et la fonction 'remove'.
+    # [x] Retirer les coins aux positions centrales, en utilisant la variable 'center_pos'.
+    for i in range(len(board)):
+        for j in range(len(board[i])):
+            if(board[i][j] == 0 and (i, j) not in special_coins_pos + center_pos):
+                coins.append((i,j))
 
     return coins
 
 def create_special_coins(board):
-    special_coins = []
-
-    # TODO: Ajouter des coins aux positions spéciales, en utilisant la variable 'special_coins_pos'.
-    
-    return special_coins
+    # [x] Ajouter des coins aux positions spéciales, en utilisant la variable 'special_coins_pos'.
+    return special_coins_pos
